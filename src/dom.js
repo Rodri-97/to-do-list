275eec5afe8ce7eccabeb99a6fd673212f851390
rodrigo <rodrigo_coelho@orange.fr>
2021-08-17 19:04:51 +0100

    toggle projects by clicking on project button

--- src/dom.js
+++ src/dom.js
@@ -20,13 +20,31 @@ export const closeForm = () => {
     addTodoForm.style.display = "none";
 }
 
-export const displayProjects = (projects) => {
+export const displayProjectsList = (projects) => {
     const projectsList = document.getElementById("projects-list");
     projectsList.innerHTML = "";
+
+    /*const formatizeID = (projectName) => {
+        const nameLower = projectName.toLowerCase();
+        if (nameLower.split(" ").length === 1) return nameLower;
+        return nameLower.split(" ").map(word => word + "-").join("");
+    }*/
+
     for (let i = 0; i < projects.length; i++) {
         const li = document.createElement("li");
         li.innerHTML = projects[i].name;
         li.className = "project-item";
+        //li.id = formatizeID(project[i].name);
         projectsList.append(li);
     }
+}
+
+export const displayDefaultProject = () => {
+    const projectName = document.getElementById("project-name");
+    projectName.textContent = "Default Project";
+}
+
+export const displayProject = (project) => {
+    const projectName = document.getElementById("project-name");
+    projectName.textContent = project.name;
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import * as DOM from "./dom.js";
 import * as createTodos from "./createTodos.js";
 import { getProjects, addDefaultProject, addNewProject } from "./createProjects.js";
 
-const displayDefaultProject = (() => {
+const displayProjects = (() => {
     addDefaultProject();
     const projects = getProjects();
-    DOM.displayProjects(projects);
+    DOM.displayProjectsList(projects);
+    DOM.displayDefaultProject();
 })();
 
 const addNewProjectEventListener = (() => {
@@ -13,15 +14,33 @@ const addNewProjectEventListener = (() => {
     newProjectBtn.addEventListener("click", function() {
         addNewProject();
         const projects = getProjects();
-        DOM.displayProjects(projects);
+        DOM.displayProjectsList(projects);
+        selectProjectEventListener();
     });
 })();
 
-const addTodoBtn = document.getElementById("add-todo-btn");
-const submitBtn = document.getElementById("form-submit");
+const selectProjectEventListener = () => {
+    const projectItems = document.getElementsByClassName("project-item");
+    const projects = getProjects();
+    for (let i = 0; i < projectItems.length; i++) {
+        const project = projects[i];
+        projectItems[i].addEventListener("click", function() {
+            DOM.displayProject(project);
+        });
+    }
+};
 
-addTodoBtn.addEventListener("click", DOM.displayForm);
-submitBtn.addEventListener("click", function() {
-    const formData = DOM.getFormData();
-    DOM.closeForm();
-});
+selectProjectEventListener();
+
+// CSS/Style: Generate random color for each project + Redify selected item
+
+const addTodo = (() => {
+    const addTodoBtn = document.getElementById("add-todo-btn");
+    const submitBtn = document.getElementById("form-submit");
+
+    addTodoBtn.addEventListener("click", DOM.displayForm);
+    submitBtn.addEventListener("click", function() {
+        const formData = DOM.getFormData();
+        DOM.closeForm();
+    });
+})();
