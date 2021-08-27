857a387d6546090f74ee7b02e3b45559ef1054b8
rodrigo <rodrigo_coelho@orange.fr>
2021-08-27 14:28:47 +0200

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -2,18 +2,29 @@ import * as DOM from "./dom.js";
 import { createProject, setAllProjects, getAllProjects, addProject, findProject } from "./projects.js";
 import { createTask, getAllTasks, findTaskObject, editTaskObject } from "./tasks.js";
 
+const editDateEvent = () => {
+    const editDates = document.getElementsByClassName("edit-due");
+
+    for (let i = 0; i < editDates.length; i++) {
+        editDates[i].addEventListener("focus", function() {
+            this.type = "date";
+        });
+    };
+};
+
+const doneEvent = (task, taskTitle) => {
+    const doneButton = task.getElementsByClassName("done-btn")[0];
+    doneButton.addEventListener("click", function() {
+        const newData = DOM.getEditData(task);
+        editTaskObject(taskTitle, newData);
+        const currentProjectName = document.getElementById("project-name").textContent;
+        const currentProject = findProject(currentProjectName);
+        DOM.displayProject(currentProject);
+    });
+};
+
 const editEvent = () => {
     const tasks = document.getElementsByClassName("task-div");
-
-    const editDateEvent = () => {
-        const editDates = document.getElementsByClassName("edit-due");
-    
-        for (let i = 0; i < editDates.length; i++) {
-            editDates[i].addEventListener("focus", function() {
-                this.type = "date";
-            });
-        };
-    };
 
     for (let i = 0; i < tasks.length; i++) {
         const task = tasks[i];
@@ -27,22 +38,24 @@ const editEvent = () => {
     };
 };
 
-const updateProjectItemsEvents = (allProjects) => {
+const addDisplayItemsEvents = (projectItems) => {
+    for (let i = 0; i < projectItems.length; i++) {
+        const item = projectItems[i];
+        item.addEventListener("click", function() {
+            DOM.redifySelectedItem(item, projectItems);
+            const project = findProject(item.textContent);
+            DOM.displayProject(project);
+            editEvent();
+        });
+    };
+};
+
+//const updateProjectItemsEvents = (allProjects) => {
+const updateProjectItemsEvents = () => {
     const projectItems = document.getElementsByClassName("project-item");
     const defaultProjectItem = projectItems[0];
     DOM.redifySelectedItem(defaultProjectItem, projectItems);
-
-    const addDisplayItemsEvents = (() => {
-        for (let i = 0; i < projectItems.length; i++) {
-            const item = projectItems[i];
-            item.addEventListener("click", function() {
-                DOM.redifySelectedItem(item, projectItems);
-                const project = findProject(item.textContent);
-                DOM.displayProject(project);
-                editEvent();
-            });
-        };
-    })();
+    addDisplayItemsEvents(projectItems);
 };
 
 const renderDefaultProject = (() => {
@@ -60,22 +73,24 @@ const renderDefaultProject = (() => {
     DOM.displayProject(defaultProject);
     const allProjects = getAllProjects();
     DOM.displayProjectsList(allProjects);
-    updateProjectItemsEvents(allProjects);
+    //updateProjectItemsEvents(allProjects);
+    updateProjectItemsEvents();
     editEvent();
 })();
+
+const createNewProject = () => {
+    const newProject = createProject(projectName);
+    addProject(newProject);
+    const allProjects = getAllProjects();
+    DOM.displayProjectsList(allProjects);
+    //updateProjectItemsEvents(allProjects);
+    updateProjectItemsEvents();
+};
 
 const newProjectEvent = (() => {
     const newProjectBtn = document.getElementById("new-project-btn");
     newProjectBtn.addEventListener("click", function() {
         const projectName = prompt("New Project's name:");
-
-        const createNewProject = () => {
-            const newProject = createProject(projectName);
-            addProject(newProject);
-            const allProjects = getAllProjects();
-            DOM.displayProjectsList(allProjects);
-            updateProjectItemsEvents(allProjects);
-        };
 
         if (projectName.trim() !== "" ) {
             createNewProject();
@@ -123,14 +138,3 @@ const submitEvent = (() => {
         };
     });
 })();
-
-const doneEvent = (task, taskTitle) => {
-    const doneButton = task.getElementsByClassName("done-btn")[0];
-    doneButton.addEventListener("click", function() {
-        const newData = DOM.getEditData(task);
-        editTaskObject(taskTitle, newData);
-        const currentProjectName = document.getElementById("project-name").textContent;
-        const currentProject = findProject(currentProjectName);
-        DOM.displayProject(currentProject);
-    });
-};
